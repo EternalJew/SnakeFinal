21cdd8ba6b05924c50e7510a68868e4df9f95efa
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-02-06 17:03:10 +0200

    Create food.h

---
+++ food.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int food = 3;
+class Food{
+public:
+void generateFood() {
+		int x = 0;
+		int y = 0;
+		do {
+
+			x = rand() % (width - 2) + 1;
+			y = rand() % (height - 2) + 1;
+
+
+		} while (map[x + y * width] != 0);
+
+
+		map[x + y * width] = -2;
+	}
+  };
+int main(){
+Food food;
+food.generateFood();
+
+return 0;
+}
